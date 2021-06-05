33381aca7f9aadb201f5adf192d86a140de9b9e9
azhg.sabyrzhan@gmail.com <azhg.sabyrzhan@gmali.com>
2021-06-06 00:31:11 +0500

    connect to database

---
+++ src/database.providers.ts
@@ -0,0 +1,20 @@
+import {Sequelize} from "sequelize-typescript";
+
+export const databaseProviders = [
+    {
+        provide: 'SEQUELIZE',
+        useFactory: async () => {
+            const sequelize = new Sequelize({
+                dialect: 'postgres',
+                host: 'localhost',
+                port: 5432,
+                username: 'postgres',
+                password: 'example',
+                database: 'nest-app',
+            });
+            sequelize.addModels([]);
+            await sequelize.sync();
+            return sequelize;
+        },
+    },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {PORT} from "./constant";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT , () => {console.log(`server is started on port = ${PORT}`)});
+  await app.enableCors();
 }
 bootstrap();
