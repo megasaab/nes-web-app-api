0b270df8c54e0b93df0e74c4510c1686f52912a4
azhg.sabyrzhan@gmail.com <azhg.sabyrzhan@gmali.com>
2021-06-13 04:45:42 +0500

    documentation for api

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,4 +1,5 @@
 import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
 
 interface  UserCreationAttrs {
     email: string;
@@ -8,18 +9,23 @@ interface  UserCreationAttrs {
 
 @Table({tableName: 'users'})
 export class User extends  Model<User, UserCreationAttrs> {
+    @ApiProperty({example: '1', description: 'unique id'})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
+    @ApiProperty({example: 'example@gmail.com', description: 'unique email'})
     @Column({type: DataType.STRING, unique: true , allowNull: false})
     email: string;
 
+    @ApiProperty({example: '12345', description: 'password'})
     @Column({type: DataType.STRING, allowNull: false})
     password: string;
 
+    @ApiProperty({example: 'true', description: 'off permission'})
     @Column({type: DataType.BOOLEAN, defaultValue: false})
     banned: boolean;
 
+    @ApiProperty({example: 'true', description: 'reason for ban'})
     @Column({type: DataType.BOOLEAN, allowNull: true})
     banReason: boolean;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import {Body, Controller, Get, Post} from '@nestjs/common';
 import {UsersService} from "./users.service";
 import {CreateUserDto} from "./dto/create-user.dto";
+import {ApiOperation, ApiResponse} from "@nestjs/swagger";
+import {User} from "./user.model";
 
 @Controller('users')
 export class UsersController {
@@ -8,11 +10,15 @@ export class UsersController {
     constructor(private userService: UsersService) {
     }
 
+    @ApiOperation({summary: 'Create user'})
+    @ApiResponse({status: 200, type: User})
     @Post()
     create(@Body() userDto: CreateUserDto) {
         return this.userService.createUser(userDto);
     }
 
+    @ApiOperation({summary: 'Get user list'})
+    @ApiResponse({status: 200, type: [User]})
     @Get()
     getAll() {
         return this.userService.getAllUsers();
