36d18ce765d3da2c502a31f7786f6860f1ae59a9
azhg.sabyrzhan@gmail.com <azhg.sabyrzhan@gmali.com>
2021-06-12 23:30:06 +0500

    refactor some modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,29 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import {databaseProviders} from "./database.providers";
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from "@nestjs/config";
+import {Module} from "@nestjs/common";
+import {AppService} from "./app.service";
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "./users/user.model";
 
 
 
 @Module({
   controllers: [AppController],
-  providers: [AppService, ...databaseProviders],
+  providers: [AppService],
   imports: [
       ConfigModule.forRoot({
           envFilePath: '.env'
+      }),
+      SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: process.env.POSTGRES_HOST,
+          port: Number(process.env.POSTGRES_PORT),
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRESS_PWD,
+          database: process.env.POSTGRES_DB,
+          models: [User],
+          autoLoadModels: true
       }),
       UsersModule
   ]
